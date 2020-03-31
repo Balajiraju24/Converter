d4e422eed143079f3fba7e5941ff81ba37f603e0
Balaji Raju Mohan <balajiraju24@gmail.com>
2020-04-01 09:07:31 +1100

    asdfasdf

--- Converter/ViewController.swift
+++ Converter/ViewController.swift
@@ -9,12 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var inputC: UITextField!
+    @IBOutlet weak var outputF: UILabel!
+    @IBAction func convert(_ sender: Any) {
+        var converted = Int(inputC.text!)!
+        converted = converted * 9/5 + 32
+        outputF.text = "\(converted) Degree Fahrenheit"
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
 
+ 
